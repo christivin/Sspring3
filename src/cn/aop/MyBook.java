3868228e56e765b61174fe600921a7233af56613
Ss <544915551@qq.com>
2018-04-12 15:17:22 +0800

    Mybook

--- src/cn/aop/MyBook.java
+++ src/cn/aop/MyBook.java
@@ -1,9 +1,18 @@
 package cn.aop;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
 /**
  * Created by Sheng on 2018/4/12.
  */
+
+@Aspect
 public class MyBook {
+
+    //在方法上面使用注解完成增强
+
+    @Before(value = "execution(* cn.aop.Book.*(..))")
     public void before1(){
         System.out.println("before............");
     }
